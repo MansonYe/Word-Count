2b5b9405a45087e438844e808f5379768983c82c
MansonYe <1097386871@qq.com>
2018-09-08 17:23:32 +0800

    ver 1.2

--- Project1/源.c
+++ Project1/源.c
@@ -33,7 +33,8 @@ void WordCount(char *Address) {
 
 	while ((word = fgetc(file)) != EOF) {
 		if ((word >= 'a' && word <= 'z') || (word >= 'A' && word <= 'Z')) {
-			if (is_word == 0) count++;
+			if (is_word == 0) 
+				count++;
 			is_word = 1;
 		}
 		else 
@@ -46,15 +47,40 @@ void WordCount(char *Address) {
 
 }
 
+void LineCount(char *Address) {
+
+	FILE *file = fopen(Address, "r");
+	assert(file != NULL);
+
+	char code;
+	int is_line = 0;
+	int count = 0;
+
+	while ((code = fgetc(file)) != EOF) {
+		if ((code != ' ') && (code != '\n') && (code != '\t')) {
+			if (is_line == 0)
+				count++;
+			is_line = 1;
+		}
+		if (code == '\n')
+			is_line = 0;
+	}
+
+	fclose(file);
+
+	printf(" line = %d", count);
+
+}
 
 int main() {
 	
 	char *Address = "test.txt";
-	char *Type = "-c";
+	char *Type = "-l";
 	
 	switch (*(Type + 1)) {
 	case 'c': CodeCount(Address); break;
 	case 'w': WordCount(Address); break;
+	case 'l': LineCount(Address); break;
 
 	}
 
