6021d144649e265a3994d223180901c360ef2252
MansonYe <1097386871@qq.com>
2018-09-14 23:43:40 +0800

    完成版

--- Project1/test4.c
+++ Project1/test4.c
@@ -1,3 +1,4 @@
+
 /*this is a test file*/
 
 /*
@@ -10,7 +11,8 @@ manson ye
 #include<string.h>
 
 
-int main() {	//main method
+int main() //main method
+{	
 	int i = 100 / 4;
 	
 	printf("hello world");/**/
